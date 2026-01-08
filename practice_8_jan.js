15c3a0aebb9445c866c2961c8701a38b429cc09a
Krishna3632 <ks5202427@gmail.com>
2026-01-08 20:19:39 +0530

    Practice

---
+++ practice_8_jan.js
@@ -0,0 +1,9 @@
+console.log("User fetching the data fdrom the server...");
+let user;
+setTimeout(() => {
+    console.log("Settineout consumed!!");
+    console.log(`User : ${user}`);
+}, 0);
+
+console.log("Nothing!!");
+
